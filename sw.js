e6a69ae7dd7db78fb5fd7a98abac3d8005fab29a
i21567etu-coder <i21567.etu@iscae.mr>
2026-03-05 00:05:40 +0000

    Update cache name to unem-pwa-v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ملف Service Worker بسيط لجعل الموقع تطبيقاً تقدمياً (PWA)
-const CACHE_NAME = 'unem-pwa-v1';
+const CACHE_NAME = 'unem-pwa-v2';
 
 self.addEventListener('install', (event) => {
     console.log('[Service Worker] تم التثبيت بنجاح');
