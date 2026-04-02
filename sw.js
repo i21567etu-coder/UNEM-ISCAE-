aa7ea915746e2cdb613293188582ba16a0f5c5c5
i21567etu-coder <i21567.etu@iscae.mr>
2026-04-02 21:42:41 +0000

    Update CACHE_NAME to version v25

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // قم بتغيير هذا الرقم في كل مرة تقوم بتحديث الموقع ليظهر الإشعار للمستخدمين
-// لتجربة النتائج، يمكنك تغييره إلى: unem-iscae-v24.1
-const CACHE_NAME = 'unem-iscae-v24';
+// لتجربة النتائج، يمكنك تغييره إلى: unem-iscae-v25
+const CACHE_NAME = 'unem-iscae-v25';
 
 const urlsToCache =[
   './',
