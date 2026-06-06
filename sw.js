207d7911614a42a4f81554e5f3a830d4cdd190c6
Mohamed Dah Agove <i21567.etu@iscae.mr>
2026-06-06 13:18:15 +0000

    Change CACHE_NAME to 'unem-iscae-V38'
    Updated cache name to trigger service worker update.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // قم بتغيير هذا الرقم في كل مرة تقوم بتحديث الموقع ليظهر الإشعار للمستخدمين
-const CACHE_NAME = 'unem-iscae-V37';
+const CACHE_NAME = 'unem-iscae-V38';
 
 const urlsToCache =[
   './',
