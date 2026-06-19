7757d0191bfb1a4756f765369c21aaeb7394f112
Mohamed Dah Agove <i21567.etu@iscae.mr>
2026-06-19 22:12:50 +0000

    Update cache name to version V48

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // قم بتغيير هذا الرقم في كل مرة تقوم بتحديث الموقع ليظهر الإشعار للمستخدمين
-const CACHE_NAME = 'unem-iscae-V47';
+const CACHE_NAME = 'unem-iscae-V48';
 
 const urlsToCache = [
   './',
